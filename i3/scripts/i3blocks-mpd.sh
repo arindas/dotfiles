351945ae458106277516e3a8a642ef0c47f3b7da
Arindam Das <dasarindam.mails@gmail.com>
2022-02-15 23:20:29 +0530

    Handled song names smaller than len

--- i3/scripts/i3blocks-mpd.sh
+++ i3/scripts/i3blocks-mpd.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 exec-mpc() {
     mpc $* &> /dev/null
 }
@@ -9,7 +8,7 @@ exec-mpc() {
 case $BLOCK_BUTTON in
   1) exec-mpc toggle ;;
   2) exec-mpc stop   ;;
-  3) showname=$((!$showname)); ;; # echo $showname > $SHOWNAME_PATH ;;
+  3) showname=$((!$showname)); ;;
   4) exec-mpc pre    ;;
   5) exec-mpc next   ;;
 esac
@@ -18,30 +17,33 @@ playing=
  paused=
 stopped=" -"
 
-song_remove_extension='
-{
+song_remove_extension='{
 	l=length($(NF)); tl=length($0);
 	printf substr($0, 0, tl-l-1)
-}
-'
+}'
 
 LENGTH=15
 
-song_format_program='
-{
+song_format_program='{
     l=length($0)
-    idx=timestamp%l
-    rdx=idx+len-l-1
-    printf "%s %s :  ", substr($0, idx, len), substr($0, 0, rdx)
-}
-'
+
+    if(l > len) {
+        idx = timestamp % l; rdx = idx + len - l - 1
+        printf "%s %s", substr($0, idx, len), substr($0, 0, rdx)
+    }
+    else { printf "%s", $0 }
+
+    printf " ━  "
+}'
 
 mpc current |
 	sed "s/&/and/" |
 	awk -F"/" '{printf $(NF)}' |
 	awk -F"." "$song_remove_extension" |
     awk -F"-" "$song_remove_extension" |
-    awk -v timestamp=$(date "+%s") -v len=$LENGTH "$song_format_program" 
+    awk -v timestamp=$(date "+%s") \
+        -v len=$LENGTH \
+        "$song_format_program"
 
 status=$(mpc status | sed -n 's/^\[\([^])]*\)\].*$/\1/p')
 case $status in
