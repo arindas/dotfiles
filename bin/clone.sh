1bb5dd01903d46ccf1c969b13054e8e5612f14c8
Arindam Das <dasarindam.mails@gmail.com>
2022-02-20 11:24:03 +0530

    Added a usage description for the clone utility

--- bin/clone.sh
+++ bin/clone.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-git clone git@"$1":"$2".git $HOME/source/"$1"/"$2"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 GIT_PROVIDER REPO_PATH"
+    echo "Clones a repository from the given git provider at the given path"
+    echo "git clone flags may be passed with \"FLAGS\" environment variable"
+    exit 1
+fi
+
+git clone $FLAGS git@"$1":"$2".git $HOME/source/"$1"/"$2"
