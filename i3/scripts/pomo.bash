49fe983f8fbd7d852f2d2911b9a9607ebaa429c4
Arindam Das <dasarindam.mails@gmail.com>
2022-05-10 21:57:43 +0530

    Updated pomo script to print color

--- i3/scripts/pomo.bash
+++ i3/scripts/pomo.bash
@@ -1,2 +1,6 @@
 #!/bin/bash
-pomo | cut -d " " -f2 -
+
+pomo_output=$(pomo)
+colr=$(echo $pomo_output | cut -d "=" -f2 - | cut -d "]" -f1 -)
+text=$(echo $pomo_output | cut -d " " -f2 -)
+printf "${text}\n\n${colr}\n"
