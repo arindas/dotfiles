a222afdb31ef7c40e57ce3f1a0c6752f6d705364
Arindam Das <dasarindam.mails@gmail.com>
2022-09-06 22:14:22 +0530

    Disabled some i3 blocks sections to reduce cpu usage.

--- i3/scripts/pomo.bash
+++ i3/scripts/pomo.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pomo_output=$(pomo)
-colr=$(echo $pomo_output | cut -d "=" -f2 - | cut -d "]" -f1 -)
-text=$(echo $pomo_output | cut -d " " -f2 -)
-printf "${text}\n\n${colr}\n"
+colr=$(echo "$pomo_output" | cut -d "=" -f2 - | cut -d "]" -f1 -)
+text=$(echo "$pomo_output" | cut -d "]" -f2 -)
+text="${text// /  }"
+printf "%s\n\n%s\n" "$text" "$colr"
