bdf6f0574b1b4db459e14107f9966f7fe66dad9e
Arindam Das <dasarindam.mails@gmail.com>
2025-01-01 10:55:07 +0530

    fix:: accesspoint script display
    - use awk $NF to select access id name from wifi network list
    - use -n instead of ! -z

--- i3/scripts/access-point.bash
+++ i3/scripts/access-point.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ACCESS_POINT=$(LANG=C nmcli dev | grep -w connected | grep wlan | awk '{print $(NF-1)}')
+ACCESS_POINT=$(LANG=C nmcli dev | grep -w connected | grep wlan | awk '{print $(NF)}')
 
-if [[ ! -z ${ACCESS_POINT} ]]; then
-	echo $ACCESS_POINT
+if [[ -n ${ACCESS_POINT} ]]; then
+	echo "$ACCESS_POINT"
 else
 	echo "--"
 fi
