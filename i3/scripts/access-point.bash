3a909d55ed2c571d76546e64ecac11fea8911bcd
Arindam Das <dasarindam.mails@gmail.com>
2025-05-05 22:04:02 +0530

    feat: remove cpu intensive widgets from i3blocks, fix awk scripts

--- i3/scripts/access-point.bash
+++ i3/scripts/access-point.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ACCESS_POINT=$(LANG=C nmcli dev | grep -w connected | grep wlan | awk '{print $(NF)}')
+ACCESS_POINT=$(LANG=C nmcli dev | grep -w connected | grep wlan | awk '{print $4}')
 
 if [[ -n ${ACCESS_POINT} ]]; then
 	echo "$ACCESS_POINT"
